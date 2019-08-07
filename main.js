7908b1f59447a3089a51d201282078c8e66ac441
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-06 18:26:31 -0700

    modif basic_node_app_answers.txt

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const greetings=require('./greeting.js');
 const introductions=require('./intro.js');
-const fs = require('fs');
+
 
 greetings.greetingEnglish();
 greetings.greetingJapanese();
@@ -14,7 +14,3 @@ introductions.introSpanish();
 introductions.introIcelandic();
 
 
-fs.writeFile('basic_node_app_answers.txt', 'utf8', (err) => {
-    if (err) throw err;
-    console.log('node answers file has been created');
-  });
